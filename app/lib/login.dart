2e34b6e0a7e0d6c767461015152ea7f39c41c590
IDIRYACINE <idiryacinesp@gmail.com>
2023-03-15 14:24:52 +0000

    dd

--- app/lib/login.dart
+++ app/lib/login.dart
@@ -1,4 +1,7 @@
+import 'package:app/scan.dart';
 import 'package:flutter/material.dart';
+
+import 'mainp.dart';
 
 class LoginPanel extends StatelessWidget {
   const LoginPanel({super.key});
@@ -10,12 +13,19 @@ class LoginPanel extends StatelessWidget {
         Image.asset("logo.png"),
         TextFormField(decoration: const InputDecoration(labelText: "Username")),
         TextFormField(decoration: const InputDecoration(labelText: "Password")),
-        MaterialButton(onPressed: () {}, child: const Text("Login ")),
+        MaterialButton(onPressed: () {
+          Navigator.pushReplacement(context,MaterialPageRoute(builder:(context) => const MainPanel()));
+        }, child: const Text("Login ")),
         const SizedBox(
           width: double.infinity,
           height: 5,
         ),
-      ]),
+        MaterialButton(onPressed:(){
+          Navigator.pushReplacement(context, MaterialPageRoute(builder :(context)=>  const ScanPanel()));
+        } ,child: const Icon(          Icons.qr_code,
+))
+        ], 
+      ),
     );
   }
 }
---
+++ app/lib/mainp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainPanel extends StatelessWidget{
+  const MainPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Text("Main Panel"),
+    );
+  }
+}
